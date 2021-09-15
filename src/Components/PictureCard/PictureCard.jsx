bf959ac1a248a79d1e0dc9a2e5436c597360a1a4
muath-khatib <mo3az.khatib@hotmail.com>
2021-09-15 21:23:59 +0300

    Filter the pictures cards using the data from input field Relates #11

--- src/Components/PictureCard/PictureCard.jsx
+++ src/Components/PictureCard/PictureCard.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 
-const PictureCard = ({data}) => {
-    return(
-         data.map((e) => (
-            <div className="picture-card">
-                <img className="card-img" src= {e.url} width="250px" alt={e.description}/>
-                <p className="card-description"> {e.description}</p>
-            </div>
-        ))
-    )
-
-}
+const PictureCard = ({ data }) => {
+  return data.map((e) => (
+    <div key={e.id} className="picture-card">
+      <img className="card-img" src={e.url} width="250px" alt={e.description} />
+      <p className="card-description"> {e.description}</p>
+    </div>
+  ));
+};
 
 export default PictureCard;
