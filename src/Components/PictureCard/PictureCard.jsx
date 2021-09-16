6d8a021d8452c1e2e187ed457d442ec34c0517bf
Noorin99 <noorin.sa.99@gmail.com>
2021-09-16 10:29:37 +0300

    handle error if there is a falsy input into a search bar Relates #22

--- src/Components/PictureCard/PictureCard.jsx
+++ src/Components/PictureCard/PictureCard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import './PictureCard.css'
 
-const PictureCard = ({data}) => {
-    return(
-         data.map((e) => (
-            <div className="picture-card">
-                <img className="card-img" src= {e.url}  alt={e.description}/>
-            </div>
-        ))
-    )}
+const PictureCard = ({ data }) => {
+  console.log(data)
+  if (data.length === 0) {
+    return (
+      <div className="picture-card">
+        <img className="card-img" 
+        src='https://www.store.africa-business.com/assets/img/no-record-found.gif' 
+        alt='No Data Founds' />
+      </div>
+    );
+  }
+  return data.map((e) => (
+    <div className="picture-card">
+      <img className="card-img" src={e.url} alt={e.description} />
+    </div>
+  ));
+};
 export default PictureCard;
